f7ac093d81f823b99759532a99670a2ad4e8a079
Daniel <daniel.maddalena9510@gmail.com>
2021-08-01 17:17:19 +0200

    news tendina

--- src/sections/AboutUs/index.jsx
+++ src/sections/AboutUs/index.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React , {useState} from 'react';
 import styles from './about.module.scss';
 import cannes from '../../assets/cannes.png';
 import berlin from '../../assets/berlin.png';
@@ -13,18 +13,50 @@ const AboutUs = () => {
         <h3>News</h3>
 
         <div className={styles.newsContainer}>
-            <div className={styles.bigNews} style={{backgroundImage: `url(${cannes})`}} role='img' alt='festival di Cannes'>
-                <h3>REACT AL FESTIVAL DI CANNES</h3>
-                <p>REACT - Rete Audiovisiva Competitività Tecnologia REACT - Rete Audiovisiva Competitività TecnologiaREACT - Rete Audiovisiva Competitività TecnologiaREACT - Rete Audiovisiva Competitività TecnologiaREACT - Rete Audiovisiva Competitività TecnologiaREACT - Rete Audiovisiva Competitività TecnologiaREACT - Rete Audiovisiva Competitività TecnologiaREACT - Rete Audiovisiva Competitività TecnologiaREACT - Rete Audiovisiva Competitività TecnologiaREACT - Rete Audiovisiva Competitività Tecnologia</p>
-            </div>
-            <div className={styles.smallNews} style={{backgroundImage: `url(${berlin})`}} role='img' alt='festival di Cannes'>
-                <h3>REACT SBARCA A BERLINO</h3>
-            </div>
-            <div className={styles.smallNews} style={{backgroundImage: `url(${ghirigori})`}} role='img' alt='festival di Cannes'>
-                <h3>IL NUOVO FILM DI GHIRIGORI</h3>
-            </div>
+            <ReviewCard 
+                img={berlin}
+                imgOpen={cannes}
+                alt={'cannes'}
+                title={'REACT AL FESTIVAL DI CANNES'}
+                desc={`REACT - REte Audiovisiva Competitività Tecnologia- nasce dalla volontà di un gruppo di sei imprese friulane afferenti al mondo della produzione e della realizzazione audiovisiva, cinematografica e televisiva che perseguono congiuntamente un duplice obiettivo: da un lato, favorire la visibilità e la competitività all'estero del network, valorizzandone le competenze, dall'altro ottimizzare e rafforzare i propri processi mediante l'acquisto di attrezzature, hardware e software che permettano di dare vita a solu- zioni sempre attuali e innovative rispetto alla costante evoluzione tecnologica dettata dal mercato.`}
+            />
+            <ReviewCard 
+                img={berlin}
+                imgOpen={cannes}
+                alt={'cannes'}
+                title={'REACT AL FESTIVAL DI CANNES'}
+                desc={`REACT - REte Audiovisiva Competitività Tecnologia- nasce dalla volontà di un gruppo di sei imprese friulane afferenti al mondo della produzione e della realizzazione audiovisiva, cinematografica e televisiva che perseguono congiuntamente un duplice obiettivo: da un lato, favorire la visibilità e la competitività all'estero del network, valorizzandone le competenze, dall'altro ottimizzare e rafforzare i propri processi mediante l'acquisto di attrezzature, hardware e software che permettano di dare vita a solu- zioni sempre attuali e innovative rispetto alla costante evoluzione tecnologica dettata dal mercato.`}
+            />
+            <ReviewCard 
+                img={berlin}
+                imgOpen={cannes}
+                alt={'cannes'}
+                title={'REACT AL FESTIVAL DI CANNES'}
+                desc={`REACT - REte Audiovisiva Competitività Tecnologia- nasce dalla volontà di un gruppo di sei imprese friulane afferenti al mondo della produzione e della realizzazione audiovisiva, cinematografica e televisiva che perseguono congiuntamente un duplice obiettivo: da un lato, favorire la visibilità e la competitività all'estero del network, valorizzandone le competenze, dall'altro ottimizzare e rafforzare i propri processi mediante l'acquisto di attrezzature, hardware e software che permettano di dare vita a solu- zioni sempre attuali e innovative rispetto alla costante evoluzione tecnologica dettata dal mercato.`}
+            />
         </div>
     </div>
+}
+
+
+const ReviewCard = ({img, imgOpen, alt, title, desc}) => {
+
+    const [isOpen, setIsOpen] = useState(false);
+
+    return (
+        <div className={`${styles.news} ${isOpen ? styles.newsOpen : ''}`} style={{backgroundImage: `url(${!isOpen ? img : imgOpen})`}} role='img' alt={alt} onClick={()=> setIsOpen(!isOpen)}>
+            <h3>{title}</h3>
+            {
+                isOpen && (
+                    <p>
+                        {desc}
+                    </p>
+                )
+            }
+        </div>
+    )
+
+
 }
 
 export default AboutUs;
